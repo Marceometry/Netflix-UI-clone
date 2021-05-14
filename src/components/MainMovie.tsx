94a3212ace9aa8b63fe63df12dd912ecad4757c0
Marcelino Teixeira <marceometry@gmail.com>
2021-05-14 18:52:07 -0300

    get and show mainMovie info

---
+++ src/components/MainMovie.tsx
@@ -0,0 +1,40 @@
+import { format, parseISO } from 'date-fns'
+import css from '../css/components/mainMovie.module.scss'
+
+export default function MainMovie({ item }) {
+    const date = format(parseISO(item.first_air_date), 'yyyy')
+    
+    const genres = []
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
+
+    return (
+        <section className={css.container} style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className={css.vertical}>
+                <div className={css.horizontal}>
+                    <h1 className={css.name}>{item.name}</h1>
+
+                    <div className={css.info}>
+                        <span className={css.points}>{item.vote_average} pontos</span>
+                        <span className={css.year}>{date}</span>
+                        <span className={css.seasons}>{item.number_of_seasons} temporada{item.number_of_seasons > 1 ? 's' : ''}</span>
+                    </div>
+                    
+                    <p className={css.description}>{item.overview}</p>
+
+                    <div className={css.buttons}>
+                        <button> <img src="/play.svg" /> Assistir</button>
+                        <button>+ Minha Lista</button>
+                    </div>
+
+                    <span className={css.genres}><strong>Gêneros:</strong> {genres.join(', ')}</span>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
+import React from 'react'
 import { GetServerSideProps } from 'next'
 import Tmdb from '../services/Tmdb'
 
+import MainMovie from '../components/MainMovie'
 import List from '../components/List'
 
 import css from '../css/home.module.scss'
 
-export default function Home({ list }) {
-
-  {console.log(list)}
+export default function Home({ list, chosenMovieInfo }) {
+  // {console.log(list)}
   
   return (
-    <div className={css.container}>
-      <h1>Netflix</h1>
-      
+    <div className={css.container}>      
+      <MainMovie item={chosenMovieInfo} />
 
       <section className={css.lists}>
         {list.map((item, key) => {
@@ -27,8 +27,14 @@ export default function Home({ list }) {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const list = await Tmdb.getHomeList()
+
+  const originals = list.filter(i => i.slug === 'originals')
+  const random = Math.floor(Math.random() * originals[0].items.results.length - 1)
+  const chosenMovie = originals[0].items.results[random]
+
+  const chosenMovieInfo = await Tmdb.getMovieInfo(chosenMovie.id, 'tv')
   
   return {
-    props: { list }
+    props: { list, chosenMovieInfo }
   }
 }
