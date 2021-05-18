0df6a1c23094bba2865ea39b0a51450e46cbe2a4
Marcelino Teixeira <marceometry@gmail.com>
2021-05-17 21:30:24 -0300

    add nprogress bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
+import Link from 'next/link'
 import css from '../css/components/header.module.scss'
 
 export default function Header({ bg }) {
     return (
         <header className={`${css.container} ${bg ? css.bg : ''}`}>
-            <a href="/">
-                <img src="/logo-netflix.png" alt="Netflix" />
-            </a>
+            <Link href="/">
+                <a>
+                    <img src="/logo-netflix.png" alt="Netflix" />
+                </a>
+            </Link>
 
-            <a href="/">
-                <img src="profile-img.png" alt=":)" />
-            </a>
+            <Link href="/">
+                <a>
+                    <img src="profile-img.png" alt=":)" />
+                </a>
+            </Link>
         </header>
     )
 }
